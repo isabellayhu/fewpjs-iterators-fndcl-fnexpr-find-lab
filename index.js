995bb0fb79880f66cf177a41b11434739070e1e1
Isabella <isabella.y.hu@gmail.com>
2022-04-19 09:03:58 +1000

    commit tree fe8f7d5c18aeffaf27f8ac66468051382350015c

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function superbowlWin(records){
+    let recordYear = records.find(({result})=>result==='W');
+    // return recordYear;
+    if (recordYear){
+        return recordYear.year;
+    }
+    // return recordYear ? recordYear.year : undefined
+}
